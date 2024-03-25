d5568482c4d959600037bb766eb2bb750c0f587d
paulyongdcl <157986220+paulyongdcl@users.noreply.github.com>
2024-03-25 20:35:57 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '精选活动', url: '/tag/精选活动' }, 
   HEO_HERO_CATEGORY_2: { title: '活动资讯', url: '/tag/活动资讯' },
-  HEO_HERO_CATEGORY_3: { title: '视频教学', url: '/tag/视频教学' },
+  HEO_HERO_CATEGORY_3: { title: '游戏教学', url: '/tag/游戏教学' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐', 
